e548792f870dac09cc1e0091548786dbc7a44f7d
Swapnil182004 <swapnilbanerjee956@gmail.com>
2025-03-15 20:23:49 +0530

    added build.sh

---
+++ newsmania/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Exit if any command fails
+set -o errexit  
+
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt  
+
+# Collect static files
+python manage.py collectstatic --noinput  
+
+# Run database migrations
+python manage.py migrate  
